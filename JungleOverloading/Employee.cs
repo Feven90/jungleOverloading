f33bd5915969cde347145dc99c859aab694b9691
unknown <f.mulugeta90@gmail.com>
2019-03-11 17:53:47 -0500

    almost done

---
+++ JungleOverloading/Employee.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JungleOverloading
+{
+    class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        //public List<string> Restaurants { get; set; }
+        // public string Food { get; set; }
+
+
+        public Employee (string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public string Eat()
+        {
+            var random = new Random();
+           List<string> Restaurantz = new List<string>();
+           Restaurantz.Add("o\'charley\'s");
+           Restaurantz.Add("slim and husky");
+            Restaurantz.Add("el camino real");
+            int index = random.Next(Restaurantz.Count);
+           return (Restaurantz[index] + " restaurant");
+         }
+
+        public string Eat(string food)
+        {
+            //Food = food;
+            return food;
+            // string foodChoice = Console.ReadLine();
+        }
+
+
+        public void Eat(List<Employee> companions)
+        {
+           
+            foreach (var companion in companions)
+            {
+                Console.WriteLine(companion.FirstName);
+            }
+
+            var random = new Random();
+            List<string> Restaurantz = new List<string>();
+            Restaurantz.Add("o\'charley\'s");
+            Restaurantz.Add("slim and husky");
+            Restaurantz.Add("el camino real");
+            int index = random.Next(Restaurantz.Count);
+            Console.WriteLine($"{FirstName} {LastName} is having lunch at {Restaurantz[index]}");
+        }
+    }
+}
---
+++ JungleOverloading/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JungleOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Employee employee1 = new Employee("Aman", "Sidamo");
+            Employee employee2 = new Employee("Maria", "Bekure");
+            Employee employee3 = new Employee("Kevin", "John");
+
+            var companions = new List<Employee>();
+            companions.Add(employee1);
+            companions.Add(employee2);
+            companions.Add(employee3);
+
+            Console.WriteLine("Enter your first and last name");
+            string userFirstName = Console.ReadLine();
+            string userLastName = Console.ReadLine();
+            Employee employeInfo = new Employee(userFirstName, userLastName);
+            //string employeeInformation = new Employee();
+            Console.WriteLine($"{userFirstName + userLastName} is eating at {employeInfo.Eat()}");
+
+            Console.WriteLine();
+            Console.WriteLine("What did you eat?");
+            string foodChoice = Console.ReadLine();
+            Console.WriteLine($"{userFirstName + userLastName} ate {employeInfo.Eat(foodChoice)}");
+
+            //employeInfo.Eat(foodChoice);
+            employeInfo.Eat(companions);
+            Console.ReadLine();
+
+            
+        }
+    }
+}
